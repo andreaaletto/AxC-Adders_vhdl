7bcb996e7e28bfcd304232e0064ceaa6f6740ee2
Andrea Aletto <andrea.aletto8@gmail.com>
2019-04-09 12:45:53 +0200

    prove vivado

---
+++ vivado/axdct_project/axdct_project.sdk/cb11_driver_baremetal/Debug/src/helloworld.d
@@ -0,0 +1,21 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../cb11_driver_baremetal_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../cb11_driver_baremetal_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../cb11_driver_baremetal_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../cb11_driver_baremetal_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../cb11_driver_baremetal_bsp/ps7_cortexa9_0/include/bspconfig.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../cb11_driver_baremetal_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../cb11_driver_baremetal_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../cb11_driver_baremetal_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../cb11_driver_baremetal_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../cb11_driver_baremetal_bsp/ps7_cortexa9_0/include/bspconfig.h:
