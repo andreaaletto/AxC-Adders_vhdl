e65d22ed7ea1b4726ad641e3f553cd13e3e28225
Andrea Aletto <andrea.aletto8@gmail.com>
2019-04-16 09:28:33 +0200

    added cell type to generic list for bc12, bas08, bas09

--- vivado/BAS09/vhdl/BAS09_axi_v1_0_S00_AXI.vhd
+++ vivado/BAS09/vhdl/BAS09_axi_v1_0_S00_AXI.vhd
@@ -25,6 +25,26 @@ entity BAS09_axi_v1_0_S00_AXI is
         nab17		: natural 			:= 0;
 		nab18		: natural 			:= 0;
 		nab19		: natural 			:= 0;
+		cell0		: natural 			:= 0;
+		cell1		: natural 			:= 0;
+		cell2		: natural 			:= 0;
+		cell3		: natural 			:= 0;
+		cell4		: natural 			:= 0;
+		cell5		: natural 			:= 0;
+		cell6		: natural 			:= 0;
+		cell7		: natural 			:= 0;
+		cell8		: natural 			:= 0;
+		cell9		: natural 			:= 0;
+		cell10		: natural 			:= 0;
+		cell11		: natural 			:= 0;
+		cell12		: natural 			:= 0;
+		cell13		: natural 			:= 0;
+		cell14		: natural 			:= 0;
+		cell15		: natural 			:= 0;
+		cell16		: natural 			:= 0;
+        cell17		: natural 			:= 0;
+		cell18		: natural 			:= 0;
+		cell19		: natural 			:= 0;
 
 		-- User parameters ends
 		-- Do not modify the parameters beyond this line
@@ -283,7 +303,27 @@ architecture arch_imp of BAS09_axi_v1_0_S00_AXI is
 			nab16		: natural 			:= 0;
 					nab17		: natural 			:= 0;
 			nab18		: natural 			:= 0;
-			nab19		: natural 			:= 0
+			nab19		: natural 			:= 0;
+			cell0		: natural 			:= 0;
+		cell1		: natural 			:= 0;
+		cell2		: natural 			:= 0;
+		cell3		: natural 			:= 0;
+		cell4		: natural 			:= 0;
+		cell5		: natural 			:= 0;
+		cell6		: natural 			:= 0;
+		cell7		: natural 			:= 0;
+		cell8		: natural 			:= 0;
+		cell9		: natural 			:= 0;
+		cell10		: natural 			:= 0;
+		cell11		: natural 			:= 0;
+		cell12		: natural 			:= 0;
+		cell13		: natural 			:= 0;
+		cell14		: natural 			:= 0;
+		cell15		: natural 			:= 0;
+		cell16		: natural 			:= 0;
+        cell17		: natural 			:= 0;
+		cell18		: natural 			:= 0;
+		cell19		: natural 			:= 0
         );
         port (
                 clk		: in   std_logic;
@@ -2199,7 +2239,27 @@ begin
 					nab16 =>            nab16,
 					nab17 =>            nab17,
 					nab18 =>            nab18,
-					nab19 =>            nab19
+					nab19 =>            nab19,
+					cell0 =>             cell0,
+					cell1 =>             cell1,
+					cell2 =>             cell2,
+					cell3 =>             cell3,
+					cell4 =>             cell4,
+					cell5 =>             cell5,
+					cell6 =>             cell6,
+					cell7 =>             cell7,
+					cell8 =>             cell8,
+					cell9 =>             cell9,
+					cell10 =>            cell10,
+					cell11 =>            cell11,
+					cell12 =>            cell12,
+					cell13 =>            cell13,
+					cell14 =>            cell14,
+					cell15 =>            cell15,
+					cell16 =>            cell16,
+					cell17 =>            cell17,
+					cell18 =>            cell18,
+					cell19 =>            cell19
         )
         port map(
             clk => S_AXI_ACLK,
